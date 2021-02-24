761b2a463cf1e479d3d2fc621dce27e4bf7bdf52
walter <walterInKitchen@outlook.com>
2021-02-24 22:39:11 +0800

    忽略掉不必要的文件

--- src/test/java/org/walterinkitchen/GrammerTest.java
+++ src/test/java/org/walterinkitchen/GrammerTest.java
@@ -13,18 +13,15 @@ import java.util.List;
 public class GrammerTest {
 
     public MongoTemplate mongoTemplate() {
-        String connection = "mongodb://root:123456@127.0.0.1:17018,127.0.0.1:17017/?authSource=admin&replicaSet=rs0&readPreference=primary";
+        String connection = "mongodb://root:123456@127.0.0.1:27001/?authSource=admin&replicaSet=rs0&readPreference=primary";
         ConnectionString connectionString = new ConnectionString(connection);
-        MongoClientSettings settings = MongoClientSettings.builder()
-                .applyConnectionString(connectionString)
-                .build();
         MongoClient client = MongoClients.create(connectionString);
 
         MongoTemplate template = new MongoTemplate(client, "test");
         return template;
     }
 
-    @Test
+//    @Test
     public void sqlTest() {
 //        String sql = "SELECT * FROM goods WHERE price > 100 && cost < price ORDER BY price DESC, cost ASC LIMIT 10, 100";
 //        String sql = "SELECT name, price FROM goods WHERE price > 100 && cost < price ORDER BY price DESC, cost ASC LIMIT 10, 100";
